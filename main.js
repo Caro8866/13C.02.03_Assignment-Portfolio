db6b75a370023d11aecabafb0169700ee7aac764
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-16 00:40:01 +0200

    progress scrollbar added

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 "use strict";
+
+import { animate, scroll, stagger, timeline, inView } from "https://cdn.skypack.dev/motion";
+
+// PAGE PROGRESS AT TOP
+scroll(animate(".progress", { scaleY: [0, 1] }));
