779d28b051897420824534fcd1c1812bb67adec5
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-16 01:04:53 +0200

    scoll to top button

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ import { animate, scroll, stagger, timeline, inView } from "https://cdn.skypack.
 
 // PAGE PROGRESS AT TOP
 scroll(animate(".progress", { scaleY: [0, 1] }));
+
+// Scroll to top feature
+const scrollToTopBtn = document.querySelector(".scrollToTop");
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+window.onscroll = function () {
+  checkScrollPosition();
+};
+
+function checkScrollPosition() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "hidden";
+  }
+}
+
+function scrollToTop() {
+  scrollTo(top);
+  console.log("scrolling");
+  scrollToTopBtn.style.display = "hidden";
+}
